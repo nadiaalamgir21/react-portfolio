11a7610eb45735f8769d5b2e5a82cb2267649cde
nadiaalamgir21 <87517265+nadiaalamgir21@users.noreply.github.com>
2022-03-13 04:28:33 -0700

    footer completed

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+//Needs links to my Github, Linkedin, and a Third Platform?
+
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+           A React site for -
+           <strong> My Full Stack Development Portfolio</strong>
+          <a href="https://github.com/nadiaalamgir21" target="_blank" rel="noreferrer">
+            Nadia Alamgir
+          </a>
+          .
+          <hr />
+          <a
+            href="https://www.linkedin.com/in/nadia-alamgir21/"
+            target="_blank" rel="noreferrer"
+          >
+            LinkedIn
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
